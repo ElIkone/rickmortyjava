6cb6e6f39fc54a688bfb9ba5e67146f843a8ff9e
Benjamin Munoz <elmaildebenja@gmail.com>
2020-12-31 10:35:33 -0800

    Feat: Add main activity

--- app/src/main/java/com/example/rickandmorty/MainActivity.java
+++ app/src/main/java/com/example/rickandmorty/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, new MainFragment()).commit();
     }
 }
