c69f30095834867406b1568d5cc47dbc9d16003a
Benjamin Munoz <elmaildebenja@gmail.com>
2021-01-03 22:34:53 -0800

    Feat: Add main activity class

--- app/src/main/java/com/example/rickandmorty/MainActivity.java
+++ app/src/main/java/com/example/rickandmorty/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.rickandmorty;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, new MainFragment()).commit();
     }
 }
